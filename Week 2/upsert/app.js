3133fd2eefade77575aa47b758ba140ab4c58fe0
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-01-22 21:02:04 +0000

    Slightly different version of doing an upsert added.

--- Week 2/upsert/app.js
+++ Week 2/upsert/app.js
@@ -4,7 +4,8 @@ MongoClient.connect('mongodb://localhost:27017/course', function (err, db) {
 	if (err) throw err;
 
 	var query = { 'student': 'Frank', 'assignment' : 'hw1' };
-	var operator = { 'student': 'Frank', 'assignment': 'hw1', 'grade': 100 };
+	// var operator = { 'student': 'Frank', 'assignment': 'hw1', 'grade': 100 };
+	var operator = { '$set': { 'date_returned': new Date() , 'grade': 100 }};
 	var options = { 'upsert': true };
 
 	db.collection('grades').update(query, operator, options, function(err, upserted) {
