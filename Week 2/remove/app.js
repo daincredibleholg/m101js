9ca5e977eee73344dc16e3051498f916367020f8
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-01-22 21:37:31 +0000

    Fixed typo in port number

--- Week 2/remove/app.js
+++ Week 2/remove/app.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27071/course', function (err, db) {
+MongoClient.connect('mongodb://localhost:27017/course', function (err, db) {
 	if (err) throw err;
 
 	var query = { 'assignment': 'hw3' };
