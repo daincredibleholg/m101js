1c9a9e52b96958f562200376f0b9d56b14f33a8f
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-01-22 19:55:27 +0000

    More 'complex' example for skip, limit and sort

--- Week 2/skip_limit_sort/app.js
+++ Week 2/skip_limit_sort/app.js
@@ -5,13 +5,25 @@ MongoClient.connect('mongodb://localhost:27017/course', function (err, db) {
 
 	var grades = db.collection('grades');
 
-	var cursor = grades.find({});
+//	var cursor = grades.find({});
 
-	cursor.skip(1);
-	cursor.limit(4);
-	cursor.sort('grade', 1);
+//	cursor.skip(1);
+//	cursor.limit(4);
 
-	cursor.skip()
+	/* The reason why the parameter is an array is that objects cood get reordered and sort respects the order of the parameters.
+	   In the example below this means, that the result is sorted by grade, ascending first and by student, descendent second. */
+	//cursor.sort([['grade', 1], ['student', -1]]);
+
+	/* The following approach is just another way of using skip, limit and sort. Again, the driver / MongoDB is taking
+	   care of the correct order (sort, skip, limit).
+	 */
+	var options = {
+		'skip': 1,
+		'limit': 4,
+		'sort': [['grade', 1], ['student', -1]]
+	};
+	var cursor = grades.find({}, [], options)
+
 	cursor.each (function(err, doc) {
 		if (err) throw err;
 
