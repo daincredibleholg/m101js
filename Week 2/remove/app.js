c6990548f5677e082fbda5635479d941ecf954a0
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-01-22 21:35:32 +0000

    Example for remove() added

---
+++ Week 2/remove/app.js
@@ -0,0 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27071/course', function (err, db) {
+	if (err) throw err;
+
+	var query = { 'assignment': 'hw3' };
+
+	db.collection('grades').remove(query, function (err, removed) {
+		if (err) throw err;
+
+		console.dir("Successfully removed " + removed + " documents!");
+
+		return db.close();
+	})
+})
